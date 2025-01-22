cedb27bad3a1ecb2c91d05b6595d0c4810085834
kunito <mskang@agos.co.kr>
2025-01-22 16:56:38 +0900

    restart_program.bat 로그 파일 소스 수정

--- WatchDog_Background/restart_program.bat
+++ WatchDog_Background/restart_program.bat
@@ -1,12 +1,9 @@
 @echo off
-tasklist | find "WatchDog_Background.exe" >nul
-if not %ERRORLEVEL%==0 start "" "C:\WatchDog\WatchDog_Background.exe"
 
-
-@echo off
-tasklist | find "WatchDog.exe" >nul
-if not %ERRORLEVEL%==0 (
+REM WatchDog_Background.exe 실행 여부 확인
+tasklist | findstr /i "WatchDog_Background.exe" >nul
+if %ERRORLEVEL% neq 0 (
+    REM 실행 중이 아니면 프로세스 시작 및 로그 남기기
     start "" "C:\WatchDog\WatchDog_Background.exe"
     echo WatchDog restarted at %date% %time% >> C:\WatchDog\restart_log.txt
 )
-
